6d162ea8793ffa7f63a98cd53736ce3337a5a782
AlanHou <1174814581@qq.com>
2019-05-17 13:09:34 +0800

    添加了XSS过滤器，修复了应用变更无法记录的问题

---
+++ src/main/java/com/alan/filter/XssFilter.java
@@ -0,0 +1,25 @@
+package com.alan.filter;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import com.alan.util.XssHttpServletRequestWrapper;
+
+public class XssFilter implements Filter {
+
+    FilterConfig filterConfig = null;
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        this.filterConfig = filterConfig;
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        chain.doFilter(new XssHttpServletRequestWrapper((HttpServletRequest) request), response);
+    }
+
+    @Override
+    public void destroy() {
+        this.filterConfig = null;
+    }
+}
--- src/main/java/com/alan/service/impl/AppServiceImpl.java
+++ src/main/java/com/alan/service/impl/AppServiceImpl.java
@@ -16,6 +16,7 @@ import java.util.List;
 public class AppServiceImpl implements AppService {
     @Autowired
     private AppDao appDao;
+    @Autowired
     private EditAppDao editAppDao;
 
     @Override
